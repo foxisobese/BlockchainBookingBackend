b5bc14f894b6a413a9cffa45e67424b0d69c07e8
foxisobese <59118407+foxisobese@users.noreply.github.com>
2023-12-10 22:49:07 +0900

    Create routes.py

---
+++ routes.py
@@ -0,0 +1,76 @@
+from flask import jsonify, request
+from flask import render_template
+from app import app, db
+from app.models import User
+from app.ethereum import transfer_tokens, transfer_tickets, connect_to_ethereum
+
+
+node_endpoint = "https://rpc.ankr.com/eth_sepolia"
+web3=connect_to_ethereum(node_endpoint)
+digicket_abi = '[{"inputs":[{"internalType":"address","name":"initialOwner","type":"address"},{"internalType":"address","name":"_usdtAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721IncorrectOwner","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721InsufficientApproval","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC721InvalidApprover","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"}],"name":"ERC721InvalidOperator","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721InvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC721InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC721InvalidSender","type":"error"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721NonexistentToken","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"OwnableInvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"OwnableUnauthorizedAccount","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"_fromTokenId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"_toTokenId","type":"uint256"}],"name":"BatchMetadataUpdate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"MetadataUpdate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"price","type":"uint256"}],"name":"TicketTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getTicketInfo","outputs":[{"components":[{"internalType":"string","name":"eventName","type":"string"},{"internalType":"string","name":"eventDate","type":"string"},{"internalType":"string","name":"seatNumber","type":"string"},{"internalType":"uint256","name":"price","type":"uint256"}],"internalType":"struct Digicket.TicketInfo","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"eventName","type":"string"},{"internalType":"string","name":"eventDate","type":"string"},{"internalType":"string","name":"seatNumber","type":"string"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"string","name":"newTokenURI","type":"string"}],"name":"mintTicket","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokenCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"usdtAmount","type":"uint256"}],"name":"transferTicket","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"usdt","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"}]'
+digicket_address = web3.to_checksum_address('0x53933c4a96F8DbFCbdB04916767F788c38bD4401')
+instance = web3.eth.contract(address=digicket_address, abi=digicket_abi)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/set_address', methods=['POST']) # owner address 설정 (임시로 flask_sqlalchemy로 DB)
+def set_address():
+    data = request.get_json()
+    username = data.get('username')
+    address = data.get('address')
+
+    user = User.query.filter_by(username=username).first()
+    if user:
+        user.address = address
+    else:
+        new_user = User(username=username, address=address)
+        db.session.add(new_user)
+
+    db.session.commit()
+    return jsonify({"message": "Address updated successfully"})
+
+@app.route('/transfer_tokens', methods=['POST']) # 특정 토큰에 대해 From, To의 username을 받아 transfer
+def transfer_tokens_route():
+    data = request.get_json()
+    usernameFrom = data.get('from')
+    usernameTo = data.get('to')
+    tokenID = data.get('tokenid')
+
+    userFrom = User.query.filter_by(username=usernameFrom).first()
+    userTo = User.query.filter_by(username=usernameTo).first()
+    if not (userFrom and userTo):
+        return jsonify({"error": "User not found"})
+
+    transfer_tokens(web3, instance, userFrom.address, userTo.address, tokenID) 
+
+    return jsonify({"message": "Tokens transferred successfully"})
+
+@app.route('/transfer_tickets', methods=['POST']) 
+def transfer_tickets_route():
+    data = request.get_json()
+    username = data.get('username')
+    tokenID = data.get('tokenid')
+    amount = data.get('amount')
+
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({"error": "User not found"})
+
+    transfer_tickets(web3, instance, user.address, tokenID, amount) 
+
+    return jsonify({"message": "Tokens transferred successfully"})
+
+@app.route('/mint_tickets', methods=['POST']) 
+def mint_tickets_route():
+    data = request.get_json()
+    eventName = data.get('event_name')
+    eventDate = data.get('event_date')
+    seatNumber = data.get('seat_number')
+    price = data.get('price')
+    newTokenUrl = data.get('new_token_url')
+
+    transfer_tickets(web3,instance, eventName, eventDate, seatNumber, price, newTokenUrl) 
+
+    return jsonify({"message": "Tokens transferred successfully"})
