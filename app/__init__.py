4bd64fd60dbb3859cada2de82247d24be5d48c7b
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-12-12 13:19:47 +0900

    Final update

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__,template_folder='templates')
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+from app import routes
